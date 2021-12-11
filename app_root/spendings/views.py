bd93b5cd106b43955ca73778a9a5eccdc03a7608
matecserven <mate.cserven@gmail.com>
2021-12-11 11:49:19 +0100

    Filter and order implemented

--- app_root/spendings/views.py
+++ app_root/spendings/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import SpendingSerializer
 from .models import SpendingORM
 
@@ -6,3 +7,15 @@ from .models import SpendingORM
 class SpendingViewSet(viewsets.ModelViewSet):
     queryset = SpendingORM.objects.all()
     serializer_class = SpendingSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['currency']
+
+    def get_queryset(self):
+        queryset = self.queryset
+        order = self.request.query_params.get('order')
+        if order is not None:
+            if order == 'asc':
+                queryset = queryset.order_by('amount')
+            elif order == 'desc':
+                queryset = queryset.order_by('amount').reverse()
+        return queryset
