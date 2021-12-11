1f4685a823c36a327bd9d7cda41be68e22854b3f
matecserven <mate.cserven@gmail.com>
2021-12-11 09:58:58 +0100

    Add spendings model

--- app_root/spendings/models.py
+++ app_root/spendings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SpendingORM(models.Model):
+    amount = models.PositiveIntegerField()
+    currency = models.CharField(max_length=3)
+    description = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} {} {}".format(self.description, self.amount, self.currency)
